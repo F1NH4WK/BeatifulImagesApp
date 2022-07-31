1687d2882fe368ea0f352d98c9fc7430e99d80d9
F1NH4WK <araujoplayer@gmail.com>
2022-07-30 23:18:09 -0300

    infinty image loading done

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, useWindowDimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, useWindowDimensions, FlatList } from 'react-native';
 import { ModalProvider } from 'react-native-modalfy';
 import { useState } from 'react';
 
 
-
 export default function App() {
 
-  const [image, setImage] = useState([1, 2, 3, 4])
+  const [image, setImage] = useState([Math.random() * 500])
 
   const {width, height} = useWindowDimensions();
+
+  const generateImage = (item) => 
+  <Image source={{uri: `https://picsum.photos/340/280?random=` + item}} 
+  style = {{...styles.imageStyle, width: width-40}}/>
 
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      {console.log(width)}
 
-      <View style = {{width: '100%', height: '6%', backgroundColor: '#27c6f1', marginBottom: 50}}>
-        <Text style = {{color: 'white', fontWeight: 'bold', fontSize: 30}}>Beatiful Images</Text>
+      <View style = {styles.textView}>
+        <Text style = {styles.textHeader}>Beatiful Images</Text>
       </View>
       <View style = {styles.imageView}>
-        {image.map(i => <Image source={{uri: `https://picsum.photos/360/250?random=` + i}} 
-        style = {{width: width-30, height: 250, borderWidth: 2, borderRadius: 15,
-        shadowOffset: {
-          width: 2,
-          height: 3,
-        }, shadowColor: 'red', shadowRadius: 20, shadowOpacity: 1}}/> )}
+        <FlatList renderItem={(item) => generateImage(item.item)}
+          data = {image}
+          onEndReached = {() => setImage([...image, Math.random() * 500])}
+          showsVerticalScrollIndicator = {false}></FlatList>
       </View>
     </View>
   );
@@ -39,8 +39,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
+  textHeader: {
+    color: '#27c6f1', 
+    fontWeight: '800', 
+    fontSize: 30,
+    opacity: 0.9,
+    textAlign: 'center',
+    textShadowColor: 'black',
+    textShadowOffset: {width: 3, height: 3},
+    textShadowRadius: 5,
+    letterSpacing: 2.5
+  },
+
+  textView: {
+    position: 'absolute',
+    width: '100%', 
+    height: '8%', 
+    // backgroundColor: '#27c6f1', 
+    marginBottom: 50,
+    elevation: 100,
+    shadowColor: '#27c6f1',
+    shadowOpacity: 1,
+    justifyContent: 'center',
+  },
+
   imageView: {
+    marginTop: '20%',
+    flexDirection: 'column',
+    height: '90%'
+  },
 
-
-  }
+  imageStyle: {
+    height: 280, 
+    borderWidth: 0.5,
+    borderRadius: 15,
+    marginBottom: 35,
+    borderColor: '#27c6f1',
+    opacity: 0.85,
+  },
 });
